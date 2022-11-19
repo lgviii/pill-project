bef7fd2ef9571d675342281f6f56e1956b67c4af
Christina Molodowtich <cmolodowitch@gmail.com>
2022-11-19 14:51:50 -0500

    app: Add service interface method to find matching pills
    pill-matcher-app: Added PillMatcherService interface method that takes all
    model outputs and attempts to find matching pills.

--- pill-matcher-app/src/main/java/edu/harvard/ext/dgmd_e14/fall_2022/pill_match/services/PillMatcherService.java
+++ pill-matcher-app/src/main/java/edu/harvard/ext/dgmd_e14/fall_2022/pill_match/services/PillMatcherService.java
@@ -13,6 +13,36 @@ public interface PillMatcherService {
 
     /**
      * <p>
+     * Attempts to find all pills matching the specified colors, shapes, and predicted imprint text groups.
+     * </p><p>
+     * This is the method that should be called by master service/REST controller once all the models have been run
+     * on a pill image.  The returned Map will include ALL possible pill matches based on the specified colors,
+     * shapes, and imprint text.
+     * </p><p>
+     * Note that if a pill has no imprint but there is some predicted imprint text, that pill will NOT be included in
+     * the results, as the presence of any predicted text is taken to mean that the pill has some imprint.<br>
+     * However, pills that DO have an imprint will still be included in the results even if there's no predicted
+     * imprint text, because the OCR prediction often fails to find any text even in pills with imprints.  These
+     * pills will have their overall accuracy score multiplied by 0.75 to place them below matches against pills that
+     * have no imprint.
+     * </p><p>
+     * Accuracy values will be in the range [0.0, 1.0].
+     * </p>
+     *
+     * @param colorMatchMap    Map linking the SPL color value to probability assigned by the model for that color
+     * @param shapeMatchMap    Map linking the SPL shape value to probability assigned by the model for that shape
+     * @param predictionGroups Collection containing predicted imprints, with each String including all the predictions
+     *                         from a single source, separated by semicolons
+     * @return Map containing each Pill that has at least some match across the assorted predictions, linked with the
+     *         combined probability that the match is correct
+     */
+    @NotNull
+    Map<Pill, Double> findMatchingPills(@NotNull @NotEmpty Map<String, Double> colorMatchMap,
+                                        @NotNull @NotEmpty Map<String, Double> shapeMatchMap,
+                                        @NotNull @NotEmpty Collection<String> predictionGroups);
+
+    /**
+     * <p>
      * Attempts to match pills from the specified list using the specified imprints.
      * </p><p>
      * The assumption is that you've
